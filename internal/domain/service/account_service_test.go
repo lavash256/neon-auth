8d96193eba78266fdf27659ce12e61967ad966f2
Pavel Eliseev <paveleliseev612@gmail.com>
2020-09-22 20:24:25 +0300

    Test fabric service

--- internal/domain/service/account_service_test.go
+++ internal/domain/service/account_service_test.go
@@ -13,10 +13,14 @@ func TestAccountService(t *testing.T) {
 	accountRepository := persistence.MemoryAccountRepository{}
 	account, err := model.NewAccount("test@test.ru", "Test")
 	_ = accountRepository.SaveAccount(account)
-	accountService := AccountService{repository: &accountRepository}
+	accountService := NewAccountService(&accountRepository)
 	err = accountService.Dublicated("new_test@test.ru")
 	assert.Equal(t, nil, err, "Since this account is not in the repository, it must return nil")
 
 	err = accountService.Dublicated("test@test.ru")
 	assert.Equal(t, "test@test.ru already exists", err.Error(), "Since a user with such an account exists, he should return an error")
+}
+
+func TestAccountServiceFabrica(t *testing.T) {
+
 }
